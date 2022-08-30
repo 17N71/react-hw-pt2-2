2826d594e7c9c20df9f2cb38e72418e79e08bc56
Narek <narek07012020@gmail.com>
2022-08-30 15:01:58 +0400

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import React, {useRef} from 'react';
+import React, {useState} from 'react';
 function App() {
-
-
-  const dataRef = useRef('')
-  const dataArray = []
-
-  window.addEventListener('keydown',function (){
-    if (event.which === 13 && dataRef.current.value.length >= 1){
-      dataArray.push(
-          {
-            id:dataArray.length,
-            title:dataRef.current.value
-          }
-      )
-      console.log(dataArray)
-      dataRef.current.value = ""
+    const [title,setTitle] = useState("")
+    const [items,setItems] = useState([])
+    function handleForm(e){
+        e.preventDefault()
+        onAdd(title)
     }
-  })
-  return (
-      <input type="text" className={'form-text'} ref={dataRef}/>
-  );
+    function onAdd (title){
+        setItems([
+            ...items,
+            {
+                id:items.length,
+                title:title
+            }
+        ])
+        console.log(items)
+        setTitle('')
+
+    }
+    function handleValue(event){
+        setTitle(event.target.value)
+    }
+    return (
+        <>
+            <form onSubmit={handleForm}>
+            <input type="text" className={'form-text'} value={title} onChange={handleValue} />
+            </form>
+                <ul>
+                    {items.map((item)=><li key={item.id}>{item.title}</li>)}
+                </ul>
+        </>
+    );
 }
 
 export default App;
